4852a997f83fd50f295d6e207e10128e2e2063db
Melanie Wong <82552071+melwong-08@users.noreply.github.com>
2022-04-07 12:10:25 -0700

    Update Table.js

--- front-end/src/tables/Table.js
+++ front-end/src/tables/Table.js
@@ -7,7 +7,16 @@ function Table({table, clearTable}){
 
     return (
         <div className="card mx-1 my-2">
-            
+            <div className="card-body bg-secondary text-light">
+                <h4 className="card-title">{table.table_name}</h4>
+                <p className="card-text">Capacity: {table.capacity}</p>
+                <p className="card-text" data-table-id-status={table.table_id}>
+                    {table.reservation_id ? "occupied" : "free"}
+                </p>
+                <div>
+                    {table.reservation_id ? <button data-table-id-finish={table.table_id} onClick={handleClick}>Finish</button> : ""}
+                </div>
+            </div>
         </div>
     )
 }
