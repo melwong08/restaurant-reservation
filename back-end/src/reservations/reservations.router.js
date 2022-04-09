e436123e1af9cd6350a23730eacd925019cdbf3b
Melanie Wong <82552071+melwong-08@users.noreply.github.com>
2022-04-08 19:03:19 -0700

    update reservation routes and service files

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -6,6 +6,18 @@
 
 const router = require("express").Router();
 const controller = require("./reservations.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+
+router
+	.route("/:reservation_id/status")
+	.put(controller.update)
+	.all(methodNotAllowed);
+
+router
+	.route("/:reservation_id")
+	.get(controller.read)
+	.put(controller.edit)
+	.all(methodNotAllowed);
 
 router
     .route("/")
--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -2,11 +2,18 @@ const knex = require("../db/connection");
 
 const tableName = "reservations";
 
-function list(date) {
+function list(date, mobile_number) {
 	if(date) {
 		return knex(tableName)
 			.select("*")
-			.where({ reservation_date: date });
+			.where({ reservation_date: date })
+			.orderBy("reservation_time", "asc");
+	}
+
+	if(mobile_number) {
+		return knex(tableName)
+			.select("*")
+			.where('mobile_number', 'like', `${mobile_number}%`);
 	}
 
 	return knex(tableName)
@@ -17,4 +24,32 @@ function create(reservation) {
 	return knex(tableName)
 		.insert(reservation)
 		.returning("*");
+}
+
+function read(reservation_id) {
+    return knex(tableName)
+        .select("*")
+        .where({ reservation_id: reservation_id })
+        .first();
+}
+
+function update(reservation_id, status) {
+    return knex(tableName)
+        .where({ reservation_id: reservation_id })
+        .update({ status: status });
+}
+
+function edit(reservation_id, reservation) {
+	return knex(tableName)
+		.where({ reservation_id: reservation_id })
+		.update({ ...reservation })
+		.returning("*");
+}
+
+module.exports = {
+	list,
+	create,
+	read,
+	update,
+	edit,
 }
