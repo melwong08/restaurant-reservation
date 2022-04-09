789dcaf87346794e8cae73f75eafa39ccaa19c7e
Melanie Wong <82552071+melwong-08@users.noreply.github.com>
2022-04-09 15:41:41 -0700

    Update 20220408181735_createTablesTable.js

--- back-end/src/db/migrations/20220408181735_createTablesTable.js
+++ back-end/src/db/migrations/20220408181735_createTablesTable.js
@@ -1,18 +1,14 @@
-
 exports.up = function(knex) {
     return knex.schema.createTable("tables", (table) => {
 		table.increments("table_id").primary().notNullable();
 		table.string("table_name").notNullable();
 		table.integer("capacity").notNullable();
 		table.string("status").notNullable();
-		
-		// let's make a foreign key here:
 		table.integer("reservation_id").unsigned();
 		table.foreign("reservation_id")
 			.references("reservation_id")
 			.inTable("reservations")
 			.onDelete("SET NULL");
-			
 		table.timestamps(true, true);
 	  });
 };
