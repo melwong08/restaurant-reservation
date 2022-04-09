c13c9b0aaad43388a72f8502507d556958398b5e
Melanie Wong <82552071+melwong-08@users.noreply.github.com>
2022-04-09 13:32:55 -0700

    Update tables.service.js

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -13,7 +13,44 @@ function create(table) {
 		.returning("*");
 }
 
+function read(table_id) {
+    return knex(tableName)
+        .select("*")
+        .where({ table_id: table_id })
+        .first();
+}
+
+function readReservation(reservation_id) {
+    return knex("reservations")
+        .select("*")
+        .where({ reservation_id: reservation_id })
+        .first();
+}
+
+function occupy(table_id, reservation_id) {
+    return knex(tableName)
+        .where({ table_id: table_id })
+        .update({ reservation_id: reservation_id , status: "occupied" });
+}
+
+function free(table_id) {
+    return knex(tableName)
+        .where({ table_id: table_id })
+        .update({ reservation_id: null, status: "free" });
+}
+
+function updateReservation(reservation_id, status) {
+    return knex("reservations")
+        .where({ reservation_id: reservation_id })
+        .update({ status: status });
+}
+
 module.exports = {
 	list,
 	create,
+    read,
+    occupy,
+    free,
+    readReservation,
+	updateReservation,
 }
